3860e13043812d31a315e631ba01aeb7884520cc
Stefan Willebrand <stefan@bricknode.com>
2022-09-01 23:03:35 +0200

    feat: added menu service

---
+++ Bricknode Broker Utilities/Contracts/Settings/UserSecrets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricknode_Broker_Utilities.Contracts.Settings
+{
+    public class UserSecrets
+    {
+        public List<BfsCredential> BfsCredentials { get; set; }
+    }
+
+    public class BfsCredential
+    {
+        public string BfsInstanceKey { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Identifier { get; set; }
+        public string ApiEndpoint { get; set; }
+    }
+}
--- Bricknode Broker Utilities/Program.cs
+++ Bricknode Broker Utilities/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BfsApi;
+using Bricknode.Soap.Sdk.Extensions;
+using Bricknode_Broker_Utilities.Contracts.Settings;
+using Bricknode_Broker_Utilities.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = BuildHost();
+        var menuService = host.Services.GetService<MenuService>();
+        await menuService.ShowMenu();
+        Console.WriteLine("Press any key to close");
+        Console.ReadKey();
+    }
+
+    private static IHost BuildHost()
+    {
+        var builder = new HostBuilder()
+            .UseEnvironment("Local")
+            .ConfigureHostConfiguration(configurationBuilder => { configurationBuilder.AddEnvironmentVariables(); })
+            .ConfigureAppConfiguration((hostContext, configurationBuilder) =>
+            {
+                configurationBuilder.AddUserSecrets<UserSecrets>();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                var userSecrets = hostContext.Configuration.GetSection("UserSecrets").Get<UserSecrets>();
+
+                var clientBuilder = services.AddMultiBfsApiClient();
+
+                foreach (var bricknodeBrokerInstance in userSecrets.BfsCredentials)
+                {
+                    clientBuilder.AddNamedBfsApiClient(bricknodeBrokerInstance.BfsInstanceKey, bfsApiConfiguration =>
+                    {
+                        bfsApiConfiguration.Credentials = new Credentials
+                        {
+                            UserName = bricknodeBrokerInstance.Username,
+                            Password = bricknodeBrokerInstance.Password
+                        };
+
+                        bfsApiConfiguration.EndpointAddress = bricknodeBrokerInstance.ApiEndpoint;
+                        bfsApiConfiguration.Identifier = bricknodeBrokerInstance.Identifier;
+                    });
+                }
+
+                AddServices(services);
+
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            });
+
+        return builder.Build();
+    }
+
+    private static void AddServices(IServiceCollection services)
+    {
+        services.AddTransient<MenuService>();
+    }
+}

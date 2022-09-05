0d778f5c19828d94e54fb90068b5efde045588fe
Stefan Willebrand <stefan@bricknode.com>
2022-09-05 23:41:14 +0200

    feat: add transaction service

---
+++ Bricknode Broker Utilities/Services/TransactionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bricknode_Broker_Utilities.Services
+{
+    public class TransactionService
+    {
+        public async Task DepositCash()
+        {
+            Console.WriteLine($"In transaction service!");
+        }
+    }
+}
